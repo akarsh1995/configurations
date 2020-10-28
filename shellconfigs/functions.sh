724a3f03d420db6e5e28d0648dff252b1ddcad39
AJ <akarsh.1995.02@gmail.com>
2020-10-28 11:04:36 +0530

    pyclean function added to clean python files.

--- shellconfigs/functions.sh
+++ shellconfigs/functions.sh
@@ -31,6 +31,16 @@ exclude () {
 cheat () {
     curl "cheat.sh/$1"
 }
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/python/python.plugin.zsh
+# Remove python compiled byte-code and mypy/pytest cache in either the current
+# directory or in a list of specified directories (including sub directories).
+pyclean() {
+    ZSH_PYCLEAN_PLACES=${*:-'.'}
+    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
+    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
+    find ${ZSH_PYCLEAN_PLACES} -depth -type d -name ".mypy_cache" -exec rm -r "{}" +
+    find ${ZSH_PYCLEAN_PLACES} -depth -type d -name ".pytest_cache" -exec rm -r "{}" +
+}
 
 create_gitignore_python() {
     echo "
