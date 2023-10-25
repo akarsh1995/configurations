c261adb622fe2055a538f875784d085dfc705c14
Akarsh Jain <akarsh.jain@gmail.com>
2023-10-25 12:00:01 +0530

    2023-10-25

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,7 @@
 starship init fish | source
+zoxide init fish | source
+set -Ux EDITOR (brew --prefix)/bin/hx
+set -x config_dir $HOME/.config
+# config files shortcuts
+alias fish_conf "$EDITOR $config_dir/fish/config.fish"
+alias alacritty_conf "$EDITOR $config_dir/alacritty/alacritty.yml"
